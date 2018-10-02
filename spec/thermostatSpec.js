121d0a47c266bdad3e7685808577ae45dce3c888
djstephenson7 <dan_0502@hotmail.co.uk>
2018-10-02 14:56:48 +0100

    Installed Power saing Mode

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -10,12 +10,32 @@ describe('Thermostat', function() {
   });
 
   it('should be able to increase the temperature', function() {
-    thermostat.increase
+    thermostat.increase()
     expect(thermostat.temperature).toEqual(21);
   });
 
   it('should be able to decrease the temperature', function() {
-    thermostat.decrease
+    thermostat.decrease()
     expect(thermostat.temperature).toEqual(19);
   });
+
+  it('should have a minimum temperature of 10 degrees', function() {
+    expect(thermostat.MINIMUM).toEqual(10);
+  });
+
+  it("shouldn't decrease temp below the minimum", function() {
+    thermostat.temperature = thermostat.MINIMUM;
+    expect(function() {
+      thermostat.decrease();
+    }).toThrowError("Can't go below minimum temp");
+  });
+
+  it('has a power saving mode which is on by default', function() {
+    expect(thermostat.powerSaving).toEqual(true)
+  })
+
+  it('can turn power saving mode off', function() {
+    thermostat.flipPowerSaving()
+    expect(thermostat.powerSaving).toEqual(false)
+  })
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,7 @@
 function Thermostat() {
   this.temperature = 20;
+  this.MINIMUM = 10;
+  this.powerSaving = true;
 }
 
 Thermostat.prototype.increase = function() {
@@ -8,4 +10,16 @@ Thermostat.prototype.increase = function() {
 
 Thermostat.prototype.decrease = function() {
   this.temperature -= 1;
+  if (this.temperature < this.MINIMUM) {
+    throw new Error("Can't go below minimum temp");
+    this.temperature = this.MINIMUM
+  }
 };
+
+Thermostat.prototype.flipPowerSaving = function() {
+  if (this.powerSaving) {
+    this.powerSaving = false
+  } else {
+    this.powerSaving = true
+  }
+}
