7ddfedbdb23d88d497aced18bbe6e38e4a893b0f
Cameron Whitehead <camjw119@gmail.com>
2018-10-02 16:26:04 +0100

    Integrateg jQuery

--- src/interface.js
+++ src/interface.js
@@ -1,4 +1,27 @@
 $(document).ready(function(){
   var thermostat = new Thermostat();
-  $("#temperature").text('thermostat.temperature');
+
+  $("#temperature").text(thermostat.temperature);
+  $("#is_power_saving").text("on.");
+
+  $("#increase_temp").click(function() {
+    thermostat.increase();
+    $("#temperature").text(thermostat.temperature);
+  })
+
+  $("#decrease_temp").click(function() {
+    thermostat.decrease();
+    $("#temperature").text(thermostat.temperature);
+  })
+
+  $("#flip_power_saving").click(function() {
+    thermostat.flipPowerSaving();
+    if (thermostat.powerSaving) {
+      $("#is_power_saving").text("on.");
+    }
+    else {
+      $("#is_power_saving").text("off.");
+    }
+  })
+
 })
