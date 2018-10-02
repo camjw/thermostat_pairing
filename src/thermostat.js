a1337f662a4215498f286660c337aee803864338
djstephenson7 <dan_0502@hotmail.co.uk>
2018-10-02 14:35:52 +0100

    Passing all three tests

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,7 +1,11 @@
 function Thermostat() {
-  this.temperature = 20
+  this.temperature = 20;
 }
 
 Thermostat.prototype.increase = function() {
-  this.temperature += 1
-}
+  this.temperature += 1;
+};
+
+Thermostat.prototype.decrease = function() {
+  this.temperature -= 1;
+};
